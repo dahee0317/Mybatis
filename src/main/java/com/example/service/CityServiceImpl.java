c7b319e60f922b88accdb7b3892b02c7d5fa2a20
DESKTOP-1QDF2O8\MZC <dahee_0317@naver.com>
2022-07-21 09:24:41 +0900

    Test SimpleDriverDataSource

---
+++ src/main/java/com/example/service/CityServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.service;
+
+import java.util.List;
+
+import com.example.vo.CityVo;
+
+public class CityServiceImpl implements CityService {
+
+	@Override
+	public CityVo selectCity(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CityVo> selectCitiesByCountryCode(String countryCode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/test/java/com/example/CityTest.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import lombok.extern.java.Log;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("classpath:beans.xml")
+@Log
+class CityTest {
+
+	@Autowired
+	private DataSource dataSource;
+	
+	@Test
+	void test() {
+		try {
+			
+		} catch(Exception ex) {
+			try {
+				Connection conn = this.dataSource.getConnection();
+				log.info(conn.toString());
+			} catch(Exception ex) {
+				log.warning(ex.getMessage());				
+			}
+		}
+		//assertNotNull("this.dataSource");
+		log.info("Hello, World");
+	}
+
+}
